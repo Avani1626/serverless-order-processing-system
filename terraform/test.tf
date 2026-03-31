089c04b0162f293e5b9bbe018777521708ac33a8
Avani1626 <avanishinde16@gmail.com>
2026-03-31 16:06:01 +0530

    Day 21: Setup Ollama and integrated AI for Terraform analysis

---
+++ terraform/test.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_policy" "bad_policy" {
+  name   = "bad-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "*",
+        Resource = "*"
+      }
+    ]
+  })
+}
